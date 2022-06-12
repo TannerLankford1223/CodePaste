f3aa5ca0374f85e98d04e68950e64c9f6cfd6ed8
TannerLankford1223 <T.Lankford730@gmail.com>
2022-06-12 03:37:40 -0500

    Initialize demo data on startup

---
+++ src/main/java/com/example/codepaste/DemoData.java
@@ -0,0 +1,31 @@
+package com.example.codepaste;
+
+import com.example.codepaste.dao.SnippetRepository;
+import com.example.codepaste.entity.CodeSnippet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DemoData implements CommandLineRunner {
+
+    @Autowired
+    private SnippetRepository snippetRepo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        snippetRepo.deleteAllInBatch();
+        List<CodeSnippet> snippets = new ArrayList<>();
+        snippets.add(new CodeSnippet("{Hello, World!}", 0, 0));
+        snippets.add(new CodeSnippet("public static void main(String[] args) {}", 0, 0));
+        snippets.add(new CodeSnippet("public class CodePaste {}", 0, 0));
+        snippets.add(new CodeSnippet("public interface CodeService {}", 100, 0));
+        snippets.add(new CodeSnippet("testCode1", 0, 100));
+        snippets.add(new CodeSnippet("testCode2", 1, 0));
+        snippets.add(new CodeSnippet("testCode3", 0, 1));
+        snippetRepo.saveAll(snippets);
+    }
+}
